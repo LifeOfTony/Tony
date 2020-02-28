e37acea23ac764e32af98263989f97110deb281a
Jiynto <48124285+Jiynto@users.noreply.github.com>
2020-02-28 17:45:00 +0000

    changed some file properties to copy.

---
+++ Tony/Tony/View.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using GeonBit.UI;
+using GeonBit.UI.Entities;
+
+namespace Tony
+{
+    static class View
+    {
+
+        static View()
+        {
+
+        }
+
+
+        static void Update(GameTime gameTime)
+        {
+            UserInterface.Active.Update(gameTime);
+        }
+
+
+        static void Draw(SpriteBatch spriteBatch)
+        {
+            UserInterface.Active.Draw(spriteBatch);
+        }
+
+
+        static void MainMenu()
+        {
+
+        }
+
+        static void CreateTextBox()
+        {
+
+        }
+    }
+}
