9d830e6b62cf255ae5cd8e3bad03364471520de8
Jiynto <48124285+Jiynto@users.noreply.github.com>
2020-04-02 01:18:36 +0100

    Added new Animation class

---
+++ Tony/Tony/Animation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tony
+{
+    public static class Animation
+    {
+
+        private static Microsoft.Xna.Framework.Content.ContentManager Content;
+
+
+        public static void setContent(Microsoft.Xna.Framework.Content.ContentManager content)
+        {
+            Content = content;
+        }
+
+        public static Texture2D AnimatePlayer(Vector2 currentPosition, Vector2 newPosition, int textureNum)
+        {
+            Texture2D newTexture;
+
+            if(currentPosition.X < newPosition.X)
+            {
+                
+            }
+            else if(currentPosition.X > newPosition.X)
+            {
+                //newTexture = Left
+            }
+            else if(currentPosition.Y < newPosition.Y)
+            {
+                //newTexture = Up
+            }
+            else if(currentPosition.Y > newPosition.Y)
+            {
+                //newTexture = Down
+            }
+            else
+            {
+                //newTexture = Idle
+            }
+
+
+
+            return newTexture;
+        }
+
+
+
+    }
+}
